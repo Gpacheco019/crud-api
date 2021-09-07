a53e86877c6cffbd2f77f3967a0662e073fe63d8
Gabriel Pacheco <gabrielpacheco.wddigital@gmail.com>
2021-09-07 15:34:42 -0300

    Prodcedures e triggers

--- src/database/migrations/20210904011943_create_table_users.js
+++ src/database/migrations/20210904011943_create_table_users.js
@@ -1,13 +1,14 @@
+const { onUpdateTrigger} = require('../../../knexfile')
 
-exports.up = knex => knex.schema.createTable('users', table => {
+exports.up = async knex => knex.schema.createTable('users', table => {
     table.increments('id')
     table.text('username').unique().notNullable()
     table.timestamp('created_at').defaultTo(knex.fn.now())
     table.timestamp('updated_at').defaultTo(knex.fn.now())
 
-  })
+  }).then(() => knex.raw(onUpdateTrigger('users')))
 
 
-exports.down = function(knex) {
+exports.down = async function(knex) {
   return knex.schema.dropTable('users')
 };
--- src/database/migrations/20210907144802_create_projects_table.js
+++ src/database/migrations/20210907144802_create_projects_table.js
@@ -1,4 +1,6 @@
-exports.up = knex => knex.schema.createTable('projects', table => {
+const { onUpdateTrigger} = require('../../../knexfile')
+
+exports.up = async knex => knex.schema.createTable('projects', table => {
   table.increments('id')
   table.text('title')
 
@@ -10,9 +12,9 @@ exports.up = knex => knex.schema.createTable('projects', table => {
 
   table.timestamp('created_at').defaultTo(knex.fn.now())
   table.timestamp('updated_at').defaultTo(knex.fn.now())
-})
+}).then(() => knex.raw(onUpdateTrigger('projects')))
 
 
-exports.down = function(knex) {
+exports.down = async function(knex) {
 return knex.schema.dropTable('projects')
 };
